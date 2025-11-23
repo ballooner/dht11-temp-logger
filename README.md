340b9358f29b7e679f68d6a0fd49c3f0c54552d6
ballooner <26182964+ballooner@users.noreply.github.com>
2025-11-23 16:54:46 +0000

    Updated README and added sendStartSignal function

--- README.md
+++ README.md
@@ -8,11 +8,12 @@ sudo ./logger
 
 # Required Hardware
 * 1 Raspberry Pi 5
-* 1 DHT11 Sensor
-* 3 MtF Jumper Cables
+* 1 DHT11 Sensor with built-in pull-up resistor
 
 # Hardware Setup
-
+Connect DHT11 + to a power pin
+Connect DHT11 OUT to a GPIO pin
+Connect DHT11 - to a ground pin
 
 # Software Setup
 Download [lgpio](https://abyz.me.uk/lg/download.html) and follow the installation guide
--- main.c
+++ main.c
@@ -1,9 +1,83 @@
 #include <stdio.h>
 #include <lgpio.h>
 
+#define GPIO_PIN 17
+
+enum ERR_E
+{
+	GPIO_OPEN_FAIL = -1,
+	GPIO_CLAIM_OUTPUT_FAIL = -2,
+	GPIO_CLAIM_INPUT_FAIL = -3,
+};
+
+// Send the start signal to the DHT11
+// Returns:
+// 0 on success
+// -1 on failure
+int sendStartSignal(int handle, int flags, int pin);
+// Read the signal sent by the DHT11
+// Returns:
+// 0 on success
+// -1 on failure
+int readSignal(int handle, int flags, int pin);
+
 int main(void)
 {
-	printf("Hello, world!\n");
+	int retVal = 0; // Program return value
+
+	// Set up basic GPIO use
+	int gpioHandle = lgGpiochipOpen(0);
+	if (gpioHandle < 0) 
+	{
+		retVal = GPIO_OPEN_FAIL;
+		goto cleanup_open_fail;
+	}
+
+	// Set on-board pull-up resistor to off
+	int flags = LG_SET_PULL_NONE;
+	
+	// Send the start signal
+	if (sendStartSignal(gpioHandle, flags, GPIO_PIN) < 0)
+	{
+		retVal = GPIO_CLAIM_OUTPUT_FAIL;
+		goto cleanup_claim_fail;
+	}
+
+	// Read the signal
+	if (readSignal(gpioHandle, flags, GPIO_PIN) < 0)
+	{
+		retVal = GPIO_CLAIM_INPUT_FAIL;
+		goto cleanup_claim_fail;
+	}
+	
+	// Program cleanup labels
+	cleanup_claim_fail:
+	lgGpiochipClose(gpioHandle);
+	cleanup_open_fail:
+	return retVal;
+}
+
+int sendStartSignal(int handle, int flags, int pin)
+{
+	// Set GPIO pin value to 0
+	if (lgGpioClaimOutput(handle, flags, pin, 0) < 0)
+	{
+		return -1;
+	}
+
+	// Sleep for 22ms to so DHT11 can register our signal
+	// (datasheet says 18ms so I added +4ms to be sure)
+	lguSleep(.022);
+
+	// Free pin
+	lgGpioFree(handle, pin);
+
+	return 0;
+}
+
+int readSignal(int handle, int flags, int pin)
+{
+	
 
 	return 0;
 }
