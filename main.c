0c3a8db2e7243e685102b16970943b2666f18ab8
ballooner <26182964+ballooner@users.noreply.github.com>
2025-11-29 18:07:43 +0000

    Created basic logging for pin switching states

--- main.c
+++ main.c
@@ -6,20 +6,18 @@
 enum ERR_E
 {
 	GPIO_OPEN_FAIL = -1,
-	GPIO_CLAIM_OUTPUT_FAIL = -2,
-	GPIO_CLAIM_INPUT_FAIL = -3,
+	GPIO_CLAIM_FAIL = -2,
+	GPIO_ALERT_FAIL = -3,
 };
+
+uint64_t previous_timestamp;
 
 // Send the start signal to the DHT11
 // Returns:
 // 0 on success
 // -1 on failure
 int sendStartSignal(int handle, int flags, int pin);
-// Read the signal sent by the DHT11
-// Returns:
-// 0 on success
-// -1 on failure
-int readSignal(int handle, int flags, int pin);
+void readSignal(int num_alerts, lgGpioAlert_p alerts, void *userdata);
 
 int main(void)
 {
@@ -39,16 +37,21 @@ int main(void)
 	// Send the start signal
 	if (sendStartSignal(gpioHandle, flags, GPIO_PIN) < 0)
 	{
-		retVal = GPIO_CLAIM_OUTPUT_FAIL;
+		retVal = GPIO_CLAIM_FAIL;
+		goto cleanup_claim_fail;
+	}
+	
+	// Read the signal
+	lgGpioSetAlertsFunc(gpioHandle, GPIO_PIN, readSignal, 0);
+
+	previous_timestamp = lguTimestamp();
+	if (lgGpioClaimAlert(gpioHandle, LG_SET_PULL_NONE, LG_BOTH_EDGES, GPIO_PIN, -1) < 0) 
+	{
+		retVal = GPIO_ALERT_FAIL;
 		goto cleanup_claim_fail;
 	}
 
-	// Read the signal
-	if (readSignal(gpioHandle, flags, GPIO_PIN) < 0)
-	{
-		retVal = GPIO_CLAIM_INPUT_FAIL;
-		goto cleanup_claim_fail;
-	}
+	lguSleep(10);
 	
 	// Program cleanup labels
 	cleanup_claim_fail:
@@ -69,15 +72,25 @@ int sendStartSignal(int handle, int flags, int pin)
 	// (datasheet says 18ms so I added +4ms to be sure)
 	lguSleep(.022);
 
+	// Set pin back to high
+	if (lgGpioWrite(handle, pin, 1) < 0)
+	{
+		return -1;
+	}
+
 	// Free pin
 	lgGpioFree(handle, pin);
-
+	
 	return 0;
 }
 
-int readSignal(int handle, int flags, int pin)
+void readSignal(int num_alerts, lgGpioAlert_p alerts, void *userdata)
 {
-	
+	for (int i = 0; i < num_alerts; i++)
+	{
+		printf("Timestamp: %"PRIu64", Level: %d, Time difference: %"PRIu64" (%d of %d)\n",
+			       	alerts[i].report.timestamp, alerts[i].report.level, (alerts[i].report.timestamp - previous_timestamp) / 1000, i + 1, num_alerts);
+	}
 
-	return 0;
+	previous_timestamp = alerts[num_alerts - 1].report.timestamp;
 }
