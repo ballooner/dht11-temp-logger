f7386f7ed172b4e87d54c7a79fd6241d70a7218e
ballooner <26182964+ballooner@users.noreply.github.com>
2025-12-22 19:15:00 +0000

    Added logging macro and started DHT11 data reading loop

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <lgpio.h>
 #include <math.h>
+
+#define LOG_MESSAGE(MESSAGE) (fprintf(stderr, "File: %s, Line: %d, Error: %s\n", __FILE__, __LINE__, MESSAGE))
 
 #define GPIO_PIN 17
 
@@ -11,41 +14,42 @@ typedef enum ERR_E
 	SUCCESS 	      = 0,
 	ERROR_GPIO_OPEN_FAIL  = -1,
 	ERROR_GPIO_CLAIM_FAIL = -2,
-	ERROR_GPIO_ALERT_FAIL = -3,
+	ERROR_GPIO_WRITE_FAIL = -3,
 } error_e;
 
 typedef struct DHT11_DATA
 {
-	int 	lastValue;
-	uint8_t intHumidity;
-	uint8_t decHumidity;
-	uint8_t intTemp;
-	uint8_t decTemp;
+	int 	lastState;
+	uint8_t  intHumidity;
+	uint8_t  decHumidity;
+	uint8_t  intTemp;
+	uint8_t  decTemp;
 } dht11_t;
 
 
 // Function Definitions
 dht11_t* dht11_create(void);
 void dht11_destroy(dht11_t* sensor);
-error_e dht11_read(dht11_t* sensor);
+error_e dht11_read_loop(dht11_t* sensor);
 
 
 int main(void)
 {
 	dht11_t* sensor = dht11_create();
 
-	while (dht11_read(sensor) == SUCCESS) lguSleep(.01); 
+	error_e retVal;
+	retVal = dht11_read_loop(sensor);
 
 	dht11_destroy(sensor);
 
-	return SUCCESS;
+	return retVal;
 }
 
 dht11_t* dht11_create(void)
 {
 	dht11_t* sensor = malloc(sizeof(dht11_t));
 
-	sensor->lastValue = 1; // DHT11 should start at high voltage because of the pull-up resistor
+	sensor->lastState = 1; 
 	sensor->intHumidity = 0;
 	sensor->decHumidity = 0;
 	sensor->intTemp = 0;
@@ -59,15 +63,67 @@ void dht11_destroy(dht11_t* sensor)
 	free(sensor);
 }
 
-error_e dht11_read(dht11_t* sensor)
+error_e dht11_read_loop(dht11_t* sensor)
 {
 	// Open the chip
-	int handle = lgGpioChipOpen(0);
+	int handle = lgGpiochipOpen(0);
+	if (handle < 0) 
+	{
+		LOG_MESSAGE("Error: Failure to open GPIO chip");
+		return ERROR_GPIO_OPEN_FAIL;
+	}
 
-	// Send start signal
-	lgGpioClaimOutput(handle, LG_SET_PULL_NONE, GPIO_PIN,  0);
-	lguSleep(.018);
-	lgGpioWrite(handle, GPIO_PIN, 1); 
+	while (1)
+	{
+		// Send start signal
+		int retVal = lgGpioClaimOutput(handle, LG_SET_PULL_NONE, GPIO_PIN,  0);
+		if (retVal < 0) 
+		{
+			LOG_MESSAGE("Error: Claiming GPIO as output failed.");
+			return ERROR_GPIO_CLAIM_FAIL;
+		}
+
+		lguSleep(.018);
+		
+		retVal = lgGpioWrite(handle, GPIO_PIN, 1); 
+		if (retVal < 0)
+		{
+			LOG_MESSAGE("Error: Writing to GPIO failed.");
+			return ERROR_GPIO_WRITE_FAIL;
+		}
+
+		sensor->lastState = 1;
+		
+		// Read data
+		retVal = lgGpioClaimInput(handle, LG_SET_PULL_NONE, GPIO_PIN);
+		if (retVal < 0) 
+		{
+			LOG_MESSAGE("Error: Claiming GPIO as input failed.");
+			return ERROR_GPIO_CLAIM_FAIL;
+		}
+
+		int bit = 0;
+		uint8_t iter = 0;
+		
+		int currState = 1;
+		while (retVal >= 0 && bit < 40)
+		{
+			if (currState == 0 && sensor->lastState == 1)
+			{
+				sensor->lastState = 0;
+				iter = 0;
+			} else if (currState == 1 && sensor->lastState == 0)
+			{
+				sensor->lastState = 1;
+				iter = 0;
+			}
+
+
+			iter += 1;
+			usleep(1);
+		}
+	}
+
 	
 
 	return SUCCESS;
